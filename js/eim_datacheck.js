474d1e12a43533e772c5f358046a8f97d1ba8d8a
Nat Kale <nathanielkale@gmail.com>
2015-03-27 14:56:47 -0700

    Simple two-column layout with a menu bar.  Shows errors on load in updated format.  Menu bar not yet functional.

--- js/eim_datacheck.js
+++ js/eim_datacheck.js
@@ -3,83 +3,68 @@
 //  to the html document.
 
 
-    var data     = [];
-    var headings = [];
+var data   = [];
+var errors = [];
 
-    var parseCSV = function(){ 
-        var fileInput = document.querySelector("#fileItem");
-        var file = fileInput.files[0];
-        
-        
-        $("#status").text("Importing and reviewing file " + file.name + "...");
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 
-        Papa.parse(file, {
-            config: {
-                     header: false,
-                     worker: true
-                     },
-            before: function(file) {
-                        console.log("Parsing file...", file);
-            },
-            error: function(err, file) {
-                        console.log("Error:", err, file);
-            },
-            complete: function(results) {
-                        
-                        data = results;
-                        console.log("Row count:" + results.data.length); 
-                        
-                        // Create header row w/ labels, and a list of labels
-                        
-                        $("#uploadedData").append('<table id="dataTable" class="pure-table-horizontal">')
-                        $("#dataTable").append('<tr id="header">')
-                        $("#header").append('<th>Errors</th>')
-                        
-                        for (var i=0; i<data.data[0].length; i++) {
-                            $("#header").append("<th>" + data.data[0][i] + "</th>");
-                            headings.push(data.data[0][i]);
-                        }
-                        
-                        // Go through each row, add DOM element & check for errors
-                        
-                        for (var i=1; i<data.data.length; i++) {
-                            $("#dataTable").append('<tr id="row' + i + '">');
-                            $("#row" + i).append('<td class="errorCount">');
-                            
-                            var rowErrors = []
-                            
-                            for (var j=0; j<data.data[i].length; j++) {
-                                field = headings[j]
-                                $("#row" + i).append('<td class="' + field + '">' + data.data[i][j] + "</td>");
-                                
-                                // Find all errors for the field (cell) we're on, add to row errors
-                                
-                                var fieldError = eimValidValue(field, data.data[i][j])
-                                
-                                if (fieldError.length > 0) {
-                                    rowErrors.push([field, fieldError]);
-                                    console.log(fieldError);
-                                    $("#row" + i + " ." + field).addClass("error");
-                                    $("#row" + i + " .errorCount").addClass("error");
-                                    
-                                    $("#row" + i + " ." + field).prop("title", fieldError);
-                                }
-                            }
-                            
-                            $("#row" + i + " .errorCount").text(rowErrors.length)
-                            
-                            if (rowErrors.length > 0) {
-                                $("#row" + i).addClass("error");
-                            }
-                            
-                        }
-                        
-                        $("#status").text("Import complete!");
-            }
-
-        });
-    }
+var parseCSV = function(){ 
+    var fileInput = document.querySelector("#fileItem");
+    var file = fileInput.files[0];
     
-    // set the input element onchange to call pullfiles
-    document.querySelector("#fileItem").onchange = parseCSV;
+    
+    $("#status").text("Importing and reviewing file " + file.name + "...");
+
+    Papa.parse(file, {
+        
+        header: true,
+        
+        error:  function(err, file) {
+            console.log("Error:", err, file);
+        },
+        
+        complete: function(results) {
+                    
+            data = results;
+            console.log("Row count:" + results.data.length); 
+            
+            
+            // Go through each row & check for errors
+            
+            $("#uploadedData").append("<ul id='errorList'></ul>");
+            
+            for (var i=0; i<data.data.length; i++) {
+                
+                var dateOfCollection = new Date(data.data[i]["Field_Collection_Start_Date"]);
+                
+                var rowName = ["Site " + data.data[i]["Location_ID"],
+                    monthNames[dateOfCollection.getMonth()] + " " + dateOfCollection.getDay() + ", " + dateOfCollection.getFullYear(),
+                    data.data[i]["Fraction_Analyzed"] + " " + data.data[i]["Result_Parameter_Name"] + " in " + data.data[i]["Sample_Matrix"]
+                    ].join(" | "); 
+                
+                // Loop through each column in the row
+                for (var property in data.data[i]) {
+                    if (data.data[i].hasOwnProperty(property)) {
+                    
+                        // Check a field in a row for errors (see eimValidator.js)
+                        var fieldError = eimValidValue(property, data.data[i][property]);
+                        
+                        if (fieldError.length > 0) {
+                            var tableLocation = "Row " + i + ", Column " + property;
+                            $("#errorList").append("<li class='error' title='" + rowName + "'>" 
+                                + tableLocation + " - " + fieldError + "</li>");
+                        };
+                    };
+                };
+            };
+            
+            $("#status").text("Import complete!");
+        }
+    });
+};
+
+// set the input element onchange to call pullfiles
+document.querySelector("#fileItem").onchange = parseCSV;
 
