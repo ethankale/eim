5306a89ffb8861894a8b1451431b4862b5843998
Nat Kale <nathanielkale@gmail.com>
2015-03-27 17:02:41 -0700

    Better formatted errors.  Added initial summaries, with functioning menu.

--- js/eim_datacheck.js
+++ js/eim_datacheck.js
@@ -4,18 +4,61 @@
 
 
 var data   = [];
-var errors = [];
 
 var monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
 
+var drawErrors = function() {
+    
+    // Clear out the main data panel and add new containers
+    $("#sidebar").empty();
+    $("#uploadedData").empty();
+    $("li#menu-summary").removeClass("pure-menu-selected");
+    
+    $("#uploadedData").append("<div id='errorSummary'></div>");
+    $("#uploadedData").append("<ul id='errorList'></ul>");
+    $("li#menu-errors").addClass("pure-menu-selected");
+    
+    // Loop through each row in the EIM template
+    var i = 0;
+    var errors = [];
+    
+    for (i; i<data.data.length; i++) {
+        
+        var dateOfCollection = new Date(data.data[i]["Field_Collection_Start_Date"]);
+        
+        var rowName = ["Site " + data.data[i]["Location_ID"],
+            monthNames[dateOfCollection.getMonth()] + " " + dateOfCollection.getDay() + ", " + dateOfCollection.getFullYear(),
+            data.data[i]["Fraction_Analyzed"] + " " + data.data[i]["Result_Parameter_Name"] + " in " + data.data[i]["Sample_Matrix"]
+            ].join(" | "); 
+        
+        // Loop through each column in the row
+        for (var property in data.data[i]) {
+            if (data.data[i].hasOwnProperty(property)) {
+            
+                // Check a field in a row for errors (see eimValidator.js)
+                var fieldError = eimValidValue(property, data.data[i][property]);
+                
+                if (fieldError.length > 0) {
+                    errors.push(fieldError);
+                    var tableLocation = "<ins>Row " + i + ", Column " + property + "</ins>";
+                    $("#errorList").append("<li class='error'>" + tableLocation + " - " + fieldError 
+                        + "<p class='rowName'>" + rowName + "</p></li>");
+                };
+            };
+        };
+    };
+    
+    // Update the menu and status div
+    
+    $("#errorSummary").append("<p>There were <strong>" + errors.length + "</strong> errors in <strong>"
+        + data.data.length + "</strong> rows. </p>");
+};
+
 var parseCSV = function(){ 
     var fileInput = document.querySelector("#fileItem");
     var file = fileInput.files[0];
-    
-    
-    $("#status").text("Importing and reviewing file " + file.name + "...");
 
     Papa.parse(file, {
         
@@ -26,45 +69,38 @@ var parseCSV = function(){
         },
         
         complete: function(results) {
-                    
-            data = results;
-            console.log("Row count:" + results.data.length); 
-            
+        
+            data = results; 
             
             // Go through each row & check for errors
+            drawErrors();
             
-            $("#uploadedData").append("<ul id='errorList'></ul>");
-            
-            for (var i=0; i<data.data.length; i++) {
-                
-                var dateOfCollection = new Date(data.data[i]["Field_Collection_Start_Date"]);
-                
-                var rowName = ["Site " + data.data[i]["Location_ID"],
-                    monthNames[dateOfCollection.getMonth()] + " " + dateOfCollection.getDay() + ", " + dateOfCollection.getFullYear(),
-                    data.data[i]["Fraction_Analyzed"] + " " + data.data[i]["Result_Parameter_Name"] + " in " + data.data[i]["Sample_Matrix"]
-                    ].join(" | "); 
-                
-                // Loop through each column in the row
-                for (var property in data.data[i]) {
-                    if (data.data[i].hasOwnProperty(property)) {
-                    
-                        // Check a field in a row for errors (see eimValidator.js)
-                        var fieldError = eimValidValue(property, data.data[i][property]);
-                        
-                        if (fieldError.length > 0) {
-                            var tableLocation = "Row " + i + ", Column " + property;
-                            $("#errorList").append("<li class='error' title='" + rowName + "'>" 
-                                + tableLocation + " - " + fieldError + "</li>");
-                        };
-                    };
-                };
-            };
-            
-            $("#status").text("Import complete!");
         }
     });
 };
 
-// set the input element onchange to call pullfiles
-document.querySelector("#fileItem").onchange = parseCSV;
+var summarizeData = function() {
+    
+    // State changes - empty out divs & change menu
+    $("#uploadedData").empty();
+    $("#sidebar").empty();
+    $("li#menu-errors").removeClass("pure-menu-selected");
+    
+    $("#sidebar").append("<h2>Parameters</h2");
+    $("#sidebar").append("<ul id='parameters'></ul>");
+    $("li#menu-summary").addClass("pure-menu-selected");
+    
+    var parameterCounts = _.countBy(data.data, function(n) { return n.Result_Parameter_Name });
+    
+    for (var parameter in parameterCounts) {
+        if (parameterCounts.hasOwnProperty(parameter)) {
+            $("ul#parameters").append("<li>" + parameter + " <span class='count'>(" + parameterCounts[parameter] + ")</span></li>");
+        };
+    };
+    
+}
+
+$("#fileItem").change(parseCSV);
+$("#menu-summary a").click(summarizeData);
+$("#menu-errors a").click(drawErrors);
 
